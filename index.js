eecf508c988399aa4c3f6b7130601977830fc64e
dloyolar <dieegolr@gmail.com>
2022-06-10 11:13:38 -0400

    [WIP] ⚙️ working with search model

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const { readInput, inquirerMenu, pause } = require('./helpers/inquirer');
+const Searches = require('./models/searches');
 
 const main = async () => {
+  const searches = new Searches();
   let option = '';
+
   do {
     option = await inquirerMenu();
+
+    switch (option) {
+      case 1:
+        const place = await readInput('Ciudad: ');
+        console.log(place);
+
+        console.log('\nInformación de la ciudad\n'.green);
+        console.log('Ciudad: ');
+        console.log('Lat:');
+        console.log('Lng:');
+        console.log('Temperatura:');
+        console.log('Mínima:');
+        console.log('Máxima:');
+        break;
+
+      default:
+        break;
+    }
 
     if (option !== 0) await pause();
   } while (option !== 0);
---
+++ models/searches.js
@@ -0,0 +1,14 @@
+class Searches {
+  history = ['Santiago', 'Los Angeles', 'California'];
+
+  constructor() {}
+
+  async city(place = '') {
+    // HTTP Request
+    console.log(place);
+
+    return []; // return cities thats match with place
+  }
+}
+
+module.exports = Searches;
